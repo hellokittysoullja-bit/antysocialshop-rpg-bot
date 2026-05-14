874e2111cb19d7b913522c25c8612b25b17f88ee
hellokittysoullja-bit <hellokittysoullja@gmail.com>
2026-05-14 15:53:12 +0300

    Hdhdhd

--- bot.py
+++ bot.py
@@ -246,7 +246,7 @@ class PlayerRepository:
                     donated, pending_transfer, lab_depth)
                 VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,
                         $17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,
-                        $30,$31,$32,$33,$34,$35,$36,$37)
+                        $30,$31,$32,$33,$34,$35,$36)
                 ON CONFLICT (user_id) DO UPDATE SET
                     username = EXCLUDED.username,
                     balance = EXCLUDED.balance,
